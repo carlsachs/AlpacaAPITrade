9ebc83f22f800cf17ae4853c6d5f0a52cfbd959a
unknown <sachscarl@gmail.com>
2021-03-04 21:30:40 -0500

    1) Created a basic sttructure for the trade-placing UI

--- alpacaui/src/components/Home.js
+++ alpacaui/src/components/Home.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+//CSS
+import "./Home.css";
+
+//Components
+import Navigation from "./Navigation";
 
 const Home = () => {
+
+    const [order, setOrder] = useState({
+        symbol: "",
+        qty: "",
+        side: "",
+        type: "",
+        time_in_force: ""
+    })
+
     return (
-    <div>
-        Home is displaying!
+    <div className="wrap">
+        <Navigation />
+        <div className="topSection">
+            <h1>Trade</h1>
+        </div>
+        <div className="tradeWrap">
+            <h2>Place an Order</h2>
+            <div className="tradeBox">
+                <form>
+                    
+                    <h5>Symbol:</h5>
+                    <input type="text" placeholder="ex. AAPL, SPY, GOOG" required />
+
+
+
+
+
+                </form>
+            </div>
+        </div>
     </div>
     )
 }
--- alpacaui/src/components/Navigation.js
+++ alpacaui/src/components/Navigation.js
@@ -1,1 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
+//css
+import "./Navigation.css";
+
+const Navigation = () => {
+    return (
+        <div className="navWrap">
+
+        </div>
+    )
+}
+
+export default Navigation;
