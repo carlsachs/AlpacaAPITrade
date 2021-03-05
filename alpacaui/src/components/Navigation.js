9871ccd8f4768d80b5c6ba055f12869b714ea995
unknown <sachscarl@gmail.com>
2021-03-04 21:52:53 -0500

    changed the styling of the Trade Box

--- alpacaui/src/components/Navigation.js
+++ alpacaui/src/components/Navigation.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import Logo from "../images/logo.png";
+
 //css
 import "./Navigation.css";
 
 const Navigation = () => {
     return (
         <div className="navWrap">
-
+            <img src={Logo} alt="Green capital A, and lowercase ui for our logo and branding" style={{height: "100px", width: "100px"}} />
         </div>
     )
 }
