779e13d4d74027fbee861bda05d96c33d5946278
unknown <sachscarl@gmail.com>
2021-03-04 21:42:16 -0500

    added initial trade box logic with the form and the useState, beginning to make it look better and hook it up with axios

--- alpacaui/src/components/Home.js
+++ alpacaui/src/components/Home.js
@@ -16,6 +16,13 @@ const Home = () => {
         time_in_force: ""
     })
 
+    const handleChange = (e) => {
+        e.preventDefault();
+        setOrder({
+            [e.target.name]: e.target.value 
+        })
+    }
+
     return (
     <div className="wrap">
         <Navigation />
@@ -28,11 +35,19 @@ const Home = () => {
                 <form>
                     
                     <h5>Symbol:</h5>
-                    <input type="text" placeholder="ex. AAPL, SPY, GOOG" required />
+                    <input type="text" placeholder="ex. AAPL, SPY, GOOG" required name="symbol" />
 
+                    <h5>Quantity:</h5>
+                    <input type="number" placeholder="ex. 500" required name="qty" />
 
+                    <h5>Side:</h5>
+                    <input type="text" placeholder="ex. buy or sell" required name="side" />
 
+                    <h5>Type:</h5>
+                    <input type="text" placeholder="ex. limit/market" required name="type" />
 
+                    <h5>Time In Force:</h5>
+                    <input type="text" placeholder="ex. day, gtc" required name="time_in_force" />
 
                 </form>
             </div>
